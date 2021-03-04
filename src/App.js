1dbc58472cbc57ae01417bcb39a7911b251740fd
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-03-04 12:19:46 +0600

    All sintacsis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Fareigit from './componet/Fareigit';
+import Inputcolor from './componet/InputColor';
+import InputLength from './componet/InputLength';
+import InputReng from './componet/InputReng';
+import Interatia from './componet/Interatia';
+import Size from './componet/Size';
+import Slaisercolor from './componet/Slaisercolor';
+import Valuta from './componet/Valuta';
+import Homework from './Homework/Homework';
+import Inputtext from './Homework/Inputtext';
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Interatia />
+      <InputLength />
+     <Inputcolor />
+     <InputReng min={0} max={200} />
+     <Valuta />
+     <Fareigit />
+     <Size />
+     <Slaisercolor />
+     <Homework min={0} max={250} />
+     
+     <Inputtext />
+     
     </div>
   );
 }
